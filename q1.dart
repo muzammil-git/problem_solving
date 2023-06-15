76afc27f8047653652676d7dfcedc4de686be645
muzammil-git <syedmuzammil67@gmail.com>
2023-06-16 02:14:29 +0500

    initial commit

---
+++ q1.dart
@@ -0,0 +1,15 @@
+//   Q.1: Create two integer variables length and breadth
+//   and assign values then
+//   check if they are square values or rectangle values.
+//   i.e: if both values are equal then it's square otherwise rectangle.
+
+void main() {
+  int length = 10;
+  int breadth = 10;
+
+  if (length == breadth) {
+    print("Its a Square");
+  } else {
+    print("Its a Rectangle");
+  }
+}
